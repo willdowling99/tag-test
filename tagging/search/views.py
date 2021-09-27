df681766686a63230be131225e53505cb2ae8dc1
Will Dowling <will.dowling@phntms.com>
2021-09-27 14:20:23 +0100

    filter based on kwarg dict

--- tagging/search/views.py
+++ tagging/search/views.py
@@ -7,22 +7,16 @@ def search(request):
   types = request.GET.getlist('types', None)
   sponsors = request.GET.getlist('sponsors', None)
   data=[]
+  tag_filters = {}
 
   if(topics):
-    topic_qs = TopicTag.objects.filter(name__in=topics)
-  else:
-    topic_qs = TopicTag.objects.all()
+    tag_filters["topic_tags__in"] = TopicTag.objects.filter(name__in=topics)
   if(types):
-    type_qs = TypeTag.objects.filter(name__in=types)
-  else:
-    type_qs = TypeTag.objects.all()
+    tag_filters["type_tags__in"] = TypeTag.objects.filter(name__in=types)
   if(sponsors):
-    sponsor_qs = SponsorTag.objects.filter(name__in=sponsors)
-  else:
-    sponsor_qs = SponsorTag.objects.all()
-
-  
-  results = BlogPage.objects.live().filter(topic_tags__in=topic_qs, type_tags__in=type_qs, sponsor_tags__in=sponsor_qs).values()
+    tag_filters["sponsor_tags__in"] = SponsorTag.objects.filter(name__in=sponsors)
+  print(tag_filters)
+  results = BlogPage.objects.live().filter(**tag_filters).values()
   count = len(results)
   data = [r for r in results]
   data.append({"count" : count})
